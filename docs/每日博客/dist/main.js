0f0a789dba5cfaa1554ac47a91d28d7caae5f8aa
yqcoder <447845126@qq.com>
2024-06-11 23:52:50 +0800

    chore: docs

--- docs/每日博客/dist/main.js
+++
@@ -1,9 +0,0 @@
-import { cutFile } from "./cutFile.js";
-
-const inputFile = document.querySelector('input[type="file"]');
-
-inputFile.onchange = async (e) => {
-  const file = e.target.files[0];
-  const chunk = await cutFile(file);
-  console.log(chunk);
-};
