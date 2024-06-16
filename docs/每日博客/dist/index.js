e1b9b56492b5112b8da3826be9ded7afb50ce46c
yqcoder <447845126@qq.com>
2024-06-16 22:15:16 +0800

    docs: init

--- docs/每日博客/dist/index.js
+++ docs/每日博客/dist/index.js
@@ -1,6 +1,46 @@
-// const name = require("./a.js");
-import { name } from "./a.js";
+// 进入全屏
+export const enter = (ele) => {
+  if (ele.requestFullscreen) {
+    ele.requestFullscreen();
+  } else if (ele.mozRequestFullscreen) {
+    ele.mozRequestFullscreen();
+  } else if (ele.webkitRequestFullscreen) {
+    ele.webkitRequestFullscreen();
+  } else if (ele.msRequestFullscreen) {
+    ele.msRequestFullscreen();
+  }
+};
 
-console.log(1);
+// 退出全屏
+export const exit = () => {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullscreen) {
+    document.mozCancelFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+};
 
-console.log(name);
+// 被全屏的元素
+export const fullEle = () => {
+  return (
+    document.fullscreenElement ||
+    document.mozFullScreenElement ||
+    document.msFullScreenElement ||
+    document.webkitFullScreenElement ||
+    null
+  );
+};
+
+// 当前是否全屏
+export const isFull = () => {
+  return !!fullEle();
+};
+
+// 全屏和非全屏切换
+export const toggle = (ele) => {
+  isFull() ? exit() : enter(ele);
+};
